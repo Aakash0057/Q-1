61b3a814602ff9226d0d8deebc108c93ce2d784f
Aakash <aakashkarthikeyan1999@gmail.com>
2026-09-02 17:04:32 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,12 @@
+def main():
+    
+    num1 = 10
+    num2 = 20
+    total = num1 + num2
+    print("--- Question 1: Python Sum Application ---")
+    print(f"Number 1: {num1}")
+    print(f"Number 2: {num2}")
+    print(f"The calculated sum is: {total}")
+
+if __name__ == "__main__":
+    main()
