d08ef95ccab8a523cb532f4ba992df79a628472f
Aakash <aakashkarthikeyan1999@gmail.com>
2026-09-02 17:07:24 +0530

    Add files via upload

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,19 @@
+pipeline {
+    agent any
+    environment {
+        PATH = "C:\\Users\\MADHAN\\AppData\\Local\\Programs\\Python\\Python312;${env.PATH}"
+    }
+    stages {
+        stage('Checkout Code') {
+            steps {
+                checkout scm
+            }
+        }
+        stage('Build') {
+            steps {
+                echo 'Executing Stage 2: Running Application...'
+                bat 'python sum.py'
+            }
+        }
+    }
+}
